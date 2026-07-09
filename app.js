af159b4061c7191e2a65303e842006f6390432b3
Gungun Verma <gungunverma670@gmail.com>
2026-07-09 14:54:03 +0530

    home page update and add animations

--- app.js
+++ app.js
@@ -10,3 +10,25 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+VANTA.NET({
+    el: "#home",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+
+    minHeight: 200,
+    minWidth: 200,
+
+    scale: 1,
+    scaleMobile: 1,
+
+    color: 0x8b5cf6,
+    backgroundColor: 0x0b0f1a,
+
+    points: 12,
+    maxDistance: 22,
+    spacing: 18,
+
+    showDots: true
+});
